cb424d02bea1b97c51069d6bb90549a4e151d057
AmadoJunior <amadoelrepalol@gmail.com>
2020-10-13 18:28:25 -0400

    Added a lerp test and some easing functions

---
+++ Lerp/cmake-build-debug/CMakeFiles/Lerp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lerp.dir/lerpTest.cpp.o"
+  "Lerp"
+  "Lerp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lerp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Lerp/lerpTest.cpp
+++ Lerp/lerpTest.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include <cmath>
+#include <math.h>
+
+float easeOutQuad(float x){
+    return 1 - (1 - x) * (1 - x);
+}
+
+float easeInQuad(float x){
+    return x * x;
+}
+
+float easeOutSine(float x){
+return sin((x * M_PI) / 2);
+}
+
+float easeInSine(float x){
+return 1 - cos((x * M_PI) / 2);
+}
+
+float lerp(float a, float b, float f){
+    return (a * (1.0 - f)) + (b * f);
+}
 
 int main(){
-    int x = 0;
-    int target = 10;
-    while(x != target){
-        std::cout << x << std::endl;
-        x = std::lerp(x, target, 0.1f);
+    float p = 0.1;
+    float a = 1;
+    float b = 11;
+    float temp;
+    while(a != b){
+        std::cout << temp << std::endl;
+        temp =  lerp(a, b, easeInQuad(p)) - a;
+        a = lerp(a, b, easeInQuad(p));
+        p += 0.1;
     }
     return 0;
 }
